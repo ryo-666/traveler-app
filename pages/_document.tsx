bce2ed5a05d5b30c14b3f64ed1a4c23515e91792
ryo-666 <r.otias.0113@gmail.com>
2022-04-02 01:16:41 +0900

    app,documentファイル修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,40 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps  } from 'next/document'
 
+type Props = {}
 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class Document extends NextDocument<Props> {
+
+  render() {
+    const title = 'タイトル'
+    const description = ''
+    const keywords = ''
+    const url = ''
+    const type = 'website'
+    const logo = '/vercel.svg'
+    const favicon = '/favicon.ico'
+
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:type" content={type} />
+          <meta property="og:url" content={url} />
+          <meta property="og:image" content={logo} />
+          <meta property="og:site_name" content={title} />
+          <link rel="canonical" href={url} />
+          <link rel="shortcut icon" href={favicon} />
+          <link rel="apple-touch-icon" href={favicon} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default Document;
