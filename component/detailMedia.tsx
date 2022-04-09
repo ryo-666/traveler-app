2ed361edc59418f17d758bbbd77e55150f82fafc
ryo-666 <r.otias.0113@gmail.com>
2022-04-09 21:58:22 +0900

    詳細ページ更新

--- component/detailMedia.tsx
+++ component/detailMedia.tsx
@@ -11,45 +11,44 @@ interface Props {
 
 const DetailMedia:FC<Props> = (props: Props) => {
 
-    useEffect(() => {
-        console.log(props);
-    })
+    // useEffect(() => {
+    //     console.log(props);
+    // })
   return (
     <>
         <Card sx={{ maxWidth: 450 }}>
             <CardMedia
               component="img"
               height="140"
-            //   image={props.item.hotel[0].hotelBasicInfo.roomImageUrl}
+              image={props.item.hotel[0].hotelBasicInfo.roomImageUrl}
               alt=""
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                {/* {props.item.hotel[0].hotelBasicInfo.hotelName} */}
+                {props.item.hotel[0].hotelBasicInfo.hotelName}
               </Typography>
               <div className="spacer"></div>
               <Typography variant="body2" color="text.secondary">
-                {/* {props.item.hotel[0].hotelBasicInfo.hotelSpecial} */}
+                {props.item.hotel[0].hotelBasicInfo.hotelSpecial}
               </Typography>
               <div className="spacer"></div>
               <Typography variant="body2" color="text.secondary">
                 ■アクセス<br />
-                {/* {props.item.hotel[0].hotelBasicInfo.access} */}
+                {props.item.hotel[0].hotelBasicInfo.access}
               </Typography>
               <div className="spacer"></div>
               <Typography variant="body2" color="text.secondary">
               ■駐車場情報<br />
-                {/* {props.item.hotel[0].hotelBasicInfo.parkingInformation} */}
+                {props.item.hotel[0].hotelBasicInfo.parkingInformation}
               </Typography>
               <div className="spacer"></div>
               <Typography variant="body2" color="text.secondary">
               ■電話番号　<br />
-                {/* {props.item.hotel[0].hotelBasicInfo.telephoneNo} */}
+                {props.item.hotel[0].hotelBasicInfo.telephoneNo}
               </Typography>
               <div className="spacer"></div>
             </CardContent>
           </Card>
-          <button onClick={() => props.onClick()}>戻る</button>
     </>
   )
 }
--- pages/detail.tsx
+++ pages/detail.tsx
@@ -1,81 +1,59 @@
-import React, { FC } from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import InputBase from '@mui/material/InputBase';
+import React, { FC, useEffect, useState } from 'react';
+import Header from '../component/header';
+import Footer from '../component/footer';
+import DetailMedia from '../component/detailMedia';
 
 interface Props {
 
 }
 
 const Detail:FC<Props> = (props: Props) => {
-  console.log(props.hotel.hotels[0].hotel[0].hotelBasicInfo.hotelName);
+  const [keywords, setKeywords] = useState("北海道");
+  const [hoteldata, setHotelData] = useState(null)
+  
+  const getInfo = async(e: React.FormEvent<HTMLFormElement>) => {
+    if (e) {
+      e.preventDefault();
+    }
+    try {
+      const url = `https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?format=json&keyword=${keywords}&applicationId=${process.env.NEXT_PUBLIC_APPLICATIONID}&hits=10`
+      const res = await fetch(url);
+      const resData = await res.json();
+      // console.log(resData);
+      const hotelsdata = resData.hotels;
+      setHotelData(hotelsdata)
+      // console.log(hotelsdata)
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  useEffect(() => {
+    getInfo(null)
+  },[])
   return (
     <>
-      <div className="box">
-      <Card sx={{ maxWidth: 550 }}>
-            <CardMedia
-              component="img"
-              height="200"
-              image={props.hotel.hotels[1].hotel[0].hotelBasicInfo.roomImageUrl}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.hotelName}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.hotelSpecial}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-                ■アクセス<br />
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.access}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-                ■最寄り駅<br />
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.nearestStation}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-              ■駐車場情報<br />
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.parkingInformation}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-              ■電話番号　<br />
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.telephoneNo}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-              ■Fax　<br />
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.faxNo}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-              ■レビュー　<br />
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.reviewAverage}
-              </Typography>
-              <div className="spacer"></div>
-              <Typography variant="body2" color="text.secondary">
-              ■ホテル情報　<br />
-                {props.hotel.hotels[0].hotel[0].hotelBasicInfo.hotelInformationUrl}
-              </Typography>
-              <div className="spacer"></div>
-            </CardContent>
-          </Card>
+      <Header />
+      <div className='main'>
+        <form onSubmit={getInfo} className="form">
+          <input type="text" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
+          <button type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 200 200"><g transform="translate(-14.472 -263.847)"><path d="M144.663,286.184a76.265,76.265,0,1,0-14.4,119.046L183.534,458.5a18.1,18.1,0,1,0,25.591-25.591l-53.269-53.268a76.265,76.265,0,0,0-11.195-93.456ZM124.12,306.728a47.211,47.211,0,1,1-66.767,0,47.212,47.212,0,0,1,66.767,0Z" transform="translate(0 0)"/></g></svg>
+          </button>
+        </form>
+        <ul>
+          {hoteldata &&
+            hoteldata.map((item, index) => (
+              <li key={index}>
+                  <a>
+                      <DetailMedia item={item}/>
+                  </a>
+              </li>
+          ))}
+        </ul>
       </div>
+      <Footer />
       <style jsx>
           {`
             .box {
@@ -85,8 +63,76 @@ const Detail:FC<Props> = (props: Props) => {
               justify-content: center;
             }
 
+            .main {
+              padding-top: 70px;
+              padding-bottom: 100px;
+            }
+
             .spacer {
               margin-bottom: 15px;
+            }
+
+            a {
+              color: inherit;
+              text-decoration: none;
+            }
+
+            ul {
+              list-style: none;
+              display: flex;
+              flex-wrap: wrap;
+            }
+
+            li {
+              list-style: none;
+              margin: 0 20px 20px 0;
+            }
+
+            .main-contants {
+              margin: 0 auto;
+              width: fit-content;
+              display: flex;
+              align-items: center;
+            }
+
+            .main-contants input {
+              margin-right: 10px;
+            }
+
+            .spacer {
+              margin-bottom: 15px;
+            }
+
+            .paging {
+              margin: 0 auto;
+              width: fit-content;
+            }
+
+            .form {
+              margin: 30px auto;
+              width: fit-content;
+              position: relative;
+            }
+
+            .form input {
+              padding: 10px 20px;
+              border: 1px solid gray;
+              border-radius: 25px;
+              font-size: 15px
+            }
+
+            .form button {
+              background-color: transparent;
+              border: none;
+              position: absolute;
+              right: 8px;
+              bottom: 5px;
+              cursor: pointer;
+              z-index: 3;
+            }
+
+            body {
+                margin: 0;
             }
           `}
       </style>
@@ -94,18 +140,5 @@ const Detail:FC<Props> = (props: Props) => {
   )
 }
 
-export const getStaticProps = async () => {
-    const res = await fetch(
-      new URL(`${process.env.NEXT_PUBLIC_API}&applicationId=${process.env.NEXT_PUBLIC_ApplicationId}`)
-    );
-    const hotel = await res.json();
-    console.log(hotel.data)
-    return {
-      props: {
-        hotel,
-      },
-    };
-
-}
 
 export default Detail
