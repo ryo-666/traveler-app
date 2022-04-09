ec787e9898f149c3090e5b42a8f6ccd9b66e871f
ryo-666 <r.otias.0113@gmail.com>
2022-04-09 19:55:25 +0900

    詳細コンポーネント追加

---
+++ component/detailMedia.tsx
@@ -0,0 +1,57 @@
+import React, { FC, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+interface Props {
+    item: any;
+    onClick?: () => void;
+}
+
+const DetailMedia:FC<Props> = (props: Props) => {
+
+    useEffect(() => {
+        console.log(props);
+    })
+  return (
+    <>
+        <Card sx={{ maxWidth: 450 }}>
+            <CardMedia
+              component="img"
+              height="140"
+            //   image={props.item.hotel[0].hotelBasicInfo.roomImageUrl}
+              alt=""
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {/* {props.item.hotel[0].hotelBasicInfo.hotelName} */}
+              </Typography>
+              <div className="spacer"></div>
+              <Typography variant="body2" color="text.secondary">
+                {/* {props.item.hotel[0].hotelBasicInfo.hotelSpecial} */}
+              </Typography>
+              <div className="spacer"></div>
+              <Typography variant="body2" color="text.secondary">
+                ■アクセス<br />
+                {/* {props.item.hotel[0].hotelBasicInfo.access} */}
+              </Typography>
+              <div className="spacer"></div>
+              <Typography variant="body2" color="text.secondary">
+              ■駐車場情報<br />
+                {/* {props.item.hotel[0].hotelBasicInfo.parkingInformation} */}
+              </Typography>
+              <div className="spacer"></div>
+              <Typography variant="body2" color="text.secondary">
+              ■電話番号　<br />
+                {/* {props.item.hotel[0].hotelBasicInfo.telephoneNo} */}
+              </Typography>
+              <div className="spacer"></div>
+            </CardContent>
+          </Card>
+          <button onClick={() => props.onClick()}>戻る</button>
+    </>
+  )
+}
+
+export default DetailMedia
